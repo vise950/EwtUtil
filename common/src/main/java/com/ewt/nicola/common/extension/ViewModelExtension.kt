bbea03068a53acc20a3d010e30bed7fe65df5515
nvisentin <nicola.visentin@enteeractive.com>
2018-12-29 21:05:44 +0100

    fix viewmodel

--- common/src/main/java/com/ewt/nicola/common/extension/ViewModelExtension.kt
+++ common/src/main/java/com/ewt/nicola/common/extension/ViewModelExtension.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 
 inline fun <reified T : ViewModel> AppCompatActivity.viewModel(crossinline f: () -> T): T {
